97a9072a05c4e489812988b1065e616f82315d0f
fclemieux <33946816+fclemieux@users.noreply.github.com>
2018-08-27 16:13:29 -0400

    Add files via upload

---
+++ project-title (2)/app-files/data.js
@@ -0,0 +1,58 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-demo5",
+      "name": "demo5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 2.7848872054879994,
+        "pitch": -0.05108929490370251,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.790491013444055,
+          "pitch": -0.06227640170669346,
+          "rotation": 12.566370614359176,
+          "target": "0-demo5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7620500149845952,
+          "pitch": 0.31933783465331445,
+          "title": "Bonhomme de neige",
+          "text": "bla bla bla bla"
+        },
+        {
+          "yaw": -2.89879354828145,
+          "pitch": -0.042190922002571796,
+          "title": "Voiture",
+          "text": "jajajaja"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
